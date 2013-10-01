995366fa329296c0d2886b75401ee59094ff3d18
Stefan Reiterer <stefan.reiterer@magnasteyr.com>
2013-10-01 08:56:46 +0200

    changed preamble of license

--- Tools.py
+++ Tools.py
@@ -1,7 +1,7 @@
 # MyMesh Module - API for easier Salome smesh usage
 # Basics.py: Smesh helper functions and classes for basic operations 
 #
-# Copyright (C) 2013  Stefan Reiterer - stefan.reiterer@magnasteyr.com or maldun.finsterschreck@gmail.com
+# Copyright (C) 2013  Stefan Reiterer - maldun.finsterschreck@gmail.com
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
