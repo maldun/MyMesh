7546f3c77e3dac8f499acabc82eaf8b653f6ed43
Stefan Reiterer <stefan.reiterer@magnasteyr.com>
2013-10-30 12:01:03 +0100

    corrections

--- Types.py
+++ Types.py
@@ -25,7 +25,8 @@ import GEOM
 import smesh
 import SMESH
 
-from numpy import array, ndarray, arange
+from numpy import array, ndarray, arange, cross
+from numpy.linalg import norm
 from numpy import float64 as data_type
 
 class Element(object):
@@ -38,6 +39,8 @@ class Element(object):
     This objects to enable better OO and functional
     programming capabilities. But we will try to minimize
     the use of self made tools as good as we can.
+
+    Checks are avoided due to the use of filters etc. so be careful.
     """
     def __init__(self,mesh,id_nr):
         """
@@ -48,7 +51,7 @@ class Element(object):
         """
         
         self.mesh = mesh
-        self.idNr = idNr  
+        self.idNr = id_nr  
         
     def getIdNr(self):
         return self.idNr
@@ -56,5 +59,62 @@ class Element(object):
     def getMesh(self):
         return self.Mesh
 
+class FaceElement(Element):
+    """
+    Template class for faces
+    """
 
+    def computeNormal(self,store = True):
+        raise NotImplementedError("Error: Not implemented!")
+
+    def getNormal(self):
+        return self.normal
+        
+    def computeArea(self,store = True):
+        raise NotImplementedError("Error: Not implemented!")
+
+    def getArea(self):
+        return self.area
+
+
+class Tria3(FaceElement):
+    """
+    Simple linear Triangles with 3 corners
+    """
+    
+    def computeNormalOp(self):
+        """
+        Computes the normal of the triangle.
+        This function only executes the computation
+        """
+
+        nodes = self.mesh.GetElemNodes(self.getIdNr())
+        p0 = self.mesh.GetNodeXYZ(nodes[0])
+        p1 = self.mesh.GetNodeXYZ(nodes[1])
+        p2 = self.mesh.GetNodeXYZ(nodes[-1])
+        
+        vec1 = array(p1) - array(p0)
+        vec2 = array(p2) - array(p0)
+        vec_normal = cross(vec1, vec2)
+        return vec_normal/norm(vec_normal), norm(vec_normal)
+
+    def computeNormal(self,store = True):
+        """
+        It stores the vector in the object per default.
+        """
+        if store:
+            self.normal = self.computeNormalOp()[0]
+        else:
+            return self.computeNormalOp()[0]
+
+    def computeArea(self,store = True):
+        """
+        It stores the vector in the object per default.
+        """
+        if store:
+            self.area = self.computeNormalOp()[1]
+        else:
+            return self.computeNormalOp()[1]
+
+        
 
